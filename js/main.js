2a2c7f7e37d46f1c3adfd21312413121a61b18c8
Madenityan <marinakorsyn@gmail.com>
2018-11-07 23:19:51 +0200

    added function myMap

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Создать свои собственные методы.
 
 // 1) myForEach - тот же самый forEach
-// 2) myMap - тот же самый map
+// 2) myMap- тот же самый map
 // 3) mySort - тот же самый sort
 // 4) myFilter - тот же самый filter
 // 5) myPush - тот же самый push
@@ -28,11 +28,33 @@ myForEach(items, item => {
 
 /*------------------------- 2 ------------------------------*/
 
+let digest = [5, 6, 7];
+
+function myMap(arr = [], callback = () => {}) {
+    let resultArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        resultArr.push(callback(arr[i], i, arr));
+    }
+
+    return resultArr;
+}
+
+let resultMap = myMap(digest,(item, index) => {
+    return item * 2;
+});
+console.log(resultMap);
+
+/*------------------------- 3 ------------------------------*/
+
+let arrSort = ['30', '40', '50', '60'];
+
+function mySort() {
 
 
+}
 
-
-
+mySort(arrSort);
 
 
 
