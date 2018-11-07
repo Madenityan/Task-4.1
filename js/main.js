c2a2fd6cdbaa093d8f28af1e2d34d68f1826cdaa
Madenityan <marinakorsyn@gmail.com>
2018-11-07 15:14:14 +0200

    added condition for a tasks

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Создать свои собственные методы.
+
+// 1) myForEach - тот же самый forEach
+// 2) myMap - тот же самый map
+// 3) mySort - тот же самый sort
+// 4) myFilter - тот же самый filter
+// 5) myPush - тот же самый push
+
+// потрібно написати всі методи, без використання цих же функцій типу, написати forEach
+// без використання самого forEach
+// // так не можна:
+// // myForEach = () => arr.forEach
+
+
+/*------------------------- 1 ------------------------------*/
+let items = [5, 10, 15];
+
+function myForEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr);
+    }
+}
+myForEach(items);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
